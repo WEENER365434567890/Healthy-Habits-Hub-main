49af2c1d2596861f7274ffda2da45699a941e529
WEENER365434567890 <70688505+WEENER365434567890@users.noreply.github.com>
2023-07-11 22:46:56 +1000

    Update activity.js

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -1,117 +1,124 @@
-(function () {
-  const nextButton = document.getElementById("activityNextPageBtn");
-  const exVal = document.querySelector('input[id="ex-choice"]');
-  const key = "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu";
-  const card1 = document.getElementById("suggestions");
+var nextButton = document.getElementById("activityNextPageBtn");
+var exVal = document.querySelector('input[id = "ex-choice"]');
+var Key = "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu";
+var card1 = document.getElementById("suggestions");
 
-  function goHome() {
-    document.location = "index.html";
-  }
+function gohome() {
+  document.location = "index.html";
+}
 
-  $(function () {
-    const handle = $("#custom-handle");
-    const availableTags = [
-      "Run",
-      "Walk",
-      "Aerobics",
-      "Rowing",
-      "Cycling",
-      "Bicycling",
-      "Skiing",
-      "Cross country",
-      "Ping pong",
-      "Tennis",
-      "Volleyball",
-      "Golf",
-      "Boxing",
-      "Baseball",
-      "Hockey",
-      "Surfing",
-      "Sailing",
-      "Bowling",
-      "Skateboarding",
-      "Diving",
-      "Frisbee",
-      "Dodgeball",
-      "Climbing",
-      "BMX",
-      "Basketball",
-      "Soccer",
-      "Football",
-    ];
+$( function() {
+    var handle = $( "#custom-handle" ); 
+    //List of words able to autocomplete   
+    var availableTags = [
+        "Run",
+        "Walk",
+        "Aerobics",
+        "Rowing",
+        "Cycling",
+        "Bicycling",
+        "Skiing",
+        "Cross country",
+        "Ping pong",
+        "Tennis",
+        "Volleyball",
+        "Golf",
+        "Boxing",
+        "Baseball",
+        "Hockey",
+        "Surfing",
+        "Sailing",
+        "Bowling",
+        "Skateboarding",
+        "Diving",
+        "Frisbee",
+        "Dodgeball",
+        "Climbing",
+        "BMX",
+        "Basketball",
+        "Soccer",
+        "Football", 
+      ];
 
-    $("#ex-choice").autocomplete({
-      source: availableTags,
-    });
+      //Identifies the id of input to autocomplete
+    $( "#ex-choice" ).autocomplete({
+        source: availableTags
+      });
 
-    $("#slider").slider({
-      value: 30,
-      max: 120,
-      create: function () {
-        handle.text($(this).slider("value"));
-      },
-      slide: function (event, ui) {
-        handle.text(ui.value);
-      },
-    });
-  });
-
-  nextButton.addEventListener("click", goHome);
-
-  function exDisplay(e) {
-    e.preventDefault();
-    if (exVal.value === "") {
-      exVal.focus();
-    } else {
-      document.getElementById("suggestions").style.display = "flex";
-      nextButton.style.display = "flex";
-      exSearch();
-    }
-  }
-
-  function exSearch() {
-    $("#suggestions").empty();
-    const val = $("#slider").slider("value");
-    const queryURL =
-      "https://api.api-ninjas.com/v1/caloriesburned?&activity=" +
-      exVal.value +
-      "&duration=" +
-      val;
-
-    fetch(queryURL, {
-      headers: { "X-Api-Key": key },
-    })
-      .then(function (response) {
-        if (!response.ok) {
-          throw new Error("Network response was not OK");
+    //Assigns current slider value to the slider for user visual
+    $( "#slider" ).slider({
+        value:30,
+        max: 120,
+        create: function() {
+            handle.text( $( this ).slider( "value" ) );
+        },
+        slide: function( event, ui ) {
+            handle.text( ui.value ); 
         }
-        return response.json();
-      })
-      .then(function (data) {
-        if (data.length === 0) {
-          const displayMessage = document.createElement("p");
+    });
+  } );
+
+nextButton.addEventListener("click", gohome);
+
+function exDisplay(e) {
+  e.preventDefault();
+  if (exVal.value === "") {
+    document.getElementById("ex-choice").focus();
+  } else document.getElementById("suggestions").style.display = "flex";
+    nextButton.style.display = "flex";
+
+  exSearch();
+}
+
+function exSearch() {
+  $("#suggestions").empty();
+  var val = $("#slider").slider("value");
+  var queryURL =
+    "https://api.api-ninjas.com/v1/caloriesburned?&activity=" +
+    exVal.value +
+    "&duration=" +
+    val;
+  fetch(queryURL, {
+    headers: { "X-Api-Key": "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu" },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+
+      let counter = 1;
+      for (let i = 0; i < 3; i++) {
+        if ((data.length === 0)) {
+          var displayMessage = document.createElement("p");
           displayMessage.setAttribute(
             "class",
             "flex items-center justify-center w-full h-1/3 leading-relaxed"
           );
           displayMessage.textContent =
-            "We couldn't find that exercise! Please try again.";
+            "We couldn't find that exercise!  Please try again.";
+  
           card1.append(displayMessage);
-        } else {
-          let counter = 1;
-          for (let i = 0; i < 3; i++) {
-            const exercise = document.createElement("p");
-            exercise.setAttribute("class", "w-full h-48 shadow-lg flex p-4");
-            exercise.textContent = `${counter}. ${data[i].name}. Great Pick! You will burn ${data[i].total_calories} calories doing this for ${val} minutes!`;
-            card1.append(exercise);
-            counter++;
-          }
         }
-      })
-      .catch(function (error) {
-        console.error("Error:", error);
-      });
-  }
 
-  document.getElementById("activity-form").addEventListener("submit", exDisplay);
-})();
+
+        var exercise1 = document.createElement("p");
+        exercise1.setAttribute(
+          "class",
+          "w-full h-48 shadow-lg flex p-4"
+        );
+        exercise1.textContent =
+          counter + '.  ' +
+          data[i].name +
+          ".  Great Pick! You will burn " +
+          data[i].total_calories +
+          " calories doing this for " +
+          val +
+          " minutes!";
+        card1.append(exercise1);
+        
+        counter++;
+      }
+    });
+}
+
+document.getElementById('activity-form').addEventListener("submit", exDisplay);
