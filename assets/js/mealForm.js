163f90dc02f2bfbf7b16c8be69aa0ccf93d37663
WEENER365434567890 <70688505+WEENER365434567890@users.noreply.github.com>
2023-07-11 22:26:46 +1000

    Update mealForm.js

--- assets/js/mealForm.js
+++ assets/js/mealForm.js
@@ -1,225 +1,162 @@
-var restrictionBTN = document.querySelectorAll('input[name="restrictions"]');
-var restrictionList = document.getElementById("restrictions");
-var allergyBTN = document.querySelectorAll('input[name="allergies"]');
-var allergyList = document.getElementById("allergies");
+const restrictionBTN = document.querySelectorAll('input[name="restrictions"]');
+const restrictionList = document.getElementById("restrictions");
+const allergyBTN = document.querySelectorAll('input[name="allergies"]');
+const allergyList = document.getElementById("allergies");
 
-var mealSxn = document.getElementById("meal-suggestions");
-var mealVal = document.querySelector('input[id = "meal-amt"]');
-var snackVal = document.querySelector('input[id = "snack-amt"]');
-var mealBtn = document.getElementById("mealBtn");
-var activityBtn = document.getElementById("activityNxBtn")
+const mealSxn = document.getElementById("meal-suggestions");
+const mealVal = document.querySelector('input[id="meal-amt"]');
+const mealBtn = document.getElementById("mealBtn");
 
-var key = "876e9a6c95694284b7cb6107cd846ea5";
+const key = "876e9a6c95694284b7cb6107cd846ea5";
 
-
-
-//restriction radio button click
+// Restriction radio button click
 function restrictionDisplay() {
-  if (document.getElementById("yesR").checked) {
-    restrictionList.style.display = "block";
-  } else {
-    restrictionList.style.display = "none";
-  }
+  restrictionList.style.display = document.getElementById("yesR").checked ? "block" : "none";
 }
 
 restrictionBTN.forEach((radio) => {
   radio.addEventListener("click", restrictionDisplay);
 });
 
-//allergy radio button click
+// Allergy radio button click
 function allergyDisplay() {
-  if (document.getElementById("yesA").checked) {
-    allergyList.style.display = "block";
-  } else {
-    allergyList.style.display = "none";
-  }
+  allergyList.style.display = document.getElementById("yesA").checked ? "block" : "none";
 }
 
 allergyBTN.forEach((radio) => {
   radio.addEventListener("click", allergyDisplay);
 });
 
-document.getElementById("activityNxBtn").onclick = function () {
-  location.href = "activityForm.html";
-};
-
+// Meal display
 function mealDisplay() {
   if (mealVal.value <= 0) {
     document.getElementById("meal-amt").focus();
   } else {
     mealSxn.style.display = "flex";
-    activityBtn.style.display ="flex";
   }
 }
 
 mealBtn.addEventListener("click", mealDisplay);
 
-
 function getAllergies() {
-  var allergyParameters = ""
-  var allergyChildren = allergyList.querySelectorAll("input")
-  for (var i = 0; i < allergyChildren.length; i++) {
-    var curElement = allergyChildren[i];
-    // console.log(curElement.value)
-    if (curElement.checked) {
-      // console.log(curElement.checked)
-      allergyParameters += curElement.value + ","
-      }
-    }
-  return allergyParameters
+  const allergyParameters = Array.from(allergyList.querySelectorAll("input"))
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.value)
+    .join(",");
+
+  return allergyParameters;
 }
 
 function getDiets() {
-  var dietParameters = ""
-  var dietChildren = restrictionList.querySelectorAll("input")
-  for (var i = 0; i < dietChildren.length; i++) {
-    var curElement = dietChildren[i];
-    // console.log(curElement.value)
-    if (curElement.checked) {
-      // console.log(curElement.checked)
-      dietParameters += curElement.value
-      console.log(dietParameters)
-      }
-    }
-  return dietParameters
+  const dietParameters = Array.from(restrictionList.querySelectorAll("input"))
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.value)
+    .join(",");
+
+  return dietParameters;
 }
 
+function getCals() {
+  const userInfo = JSON.parse(localStorage.getItem("localUser"));
+  const dietCal = userInfo.dietCal;
+  const maxMealCal = Math.round(dietCal / mealVal.value);
 
-function getCals(){
-  var userInfo = JSON.parse(localStorage.getItem("localUser"))
-  var dietCal = userInfo.dietCal
-  var maxMealCal = (Math.round(dietCal/mealVal.value))
-  
-  return maxMealCal
+  return maxMealCal;
 }
 
-function randomMeal(){
-  var random = Math.floor(Math.random() * 10)+1
- 
-  return random;
+function randomMeal() {
+  return Math.floor(Math.random() * 10) + 1;
 }
-
 
 function renderMeals(data) {
-  // console.log(data.results[0].title)
-  var suggestionBox = document.getElementById("meal-suggestions")
-  var mealContainer = document.getElementById('meal-container');
+  const suggestionBox = document.getElementById("meal-suggestions");
   suggestionBox.innerHTML = "";
 
-  if (mealVal.value < 7) {
-    for (var i = 0; i < mealVal.value; i++) {
-      var recipe = data.results[i];
-      //Create main div for all meal suggestions styling
-      var div = document.createElement("div");
-      div.classList.add("w-full", 'lg:w-1/3', "h-48", "shadow-lg", "flex", "p-4")
-      suggestionBox.appendChild(div);
-  
-  
-      // Create two div boxes for styling the images and text seperately
-      var divImg = document.createElement("div");
-      divImg.classList.add("w-1/3", "h-48",)
-      div.appendChild(divImg)
-      var divContent = document.createElement("div");
-      divContent.classList.add("w-2/3", "h-48",)
-      div.appendChild(divContent)
-  
-  
-      
-      //create specific elements for img div and content div respectivly
-      var img = document.createElement("img");
-      var h1 = document.createElement("h1");
-      var p = document.createElement("p");
-  
-      // assign pieces of content values
-      h1.textContent = recipe.title;
-      p.textContent = "Calories: " + data.results[i].nutrition.nutrients[0].amount + " kcal"
-      img.src = recipe.image
-  
-      
-      img.classList.add("w-36", "h-36", "rounded-lg")
-      h1.classList.add("pl-4","pt-4", "text-lg", "font-bold", "header-font")
-      p.classList.add("pl-4","pt-4", "body-font")
-  
-      divImg.appendChild(img);
-      divContent.appendChild(h1);
-      divContent.appendChild(p);
-    }
-  } else {
-    for (var i = 0; i < 6; i++) {
-      var recipe = data.results[i];
-      //Create main div for all meal suggestions styling
-      var div = document.createElement("div");
-      div.classList.add("w-full", "h-48", "shadow-lg", "flex", "p-4")
-      suggestionBox.appendChild(div);
-  
-  
-      // Create two div boxes for styling the images and text seperately
-      var divImg = document.createElement("div");
-      divImg.classList.add("w-1/3", "h-48",)
-      div.appendChild(divImg)
-      var divContent = document.createElement("div");
-      divContent.classList.add("w-2/3", "h-48",)
-      div.appendChild(divContent)
-  
-  
-      
-      //create specific elements for img div and content div respectivly
-      var img = document.createElement("img");
-      var h1 = document.createElement("h1");
-      var p = document.createElement("p");
-  
-      // assign pieces of content values
-      h1.textContent = recipe.title;
-      p.textContent = "Calories: " + data.results[i].nutrition.nutrients[0].amount + " kcal"
-      img.src = recipe.image
-  
-      
-      img.classList.add("w-36", "h-36", "rounded-lg")
-      h1.classList.add("pl-4","pt-4", "text-lg", "font-bold", "header-font")
-      p.classList.add("pl-4","pt-4", "body-font")
-  
-      divImg.appendChild(img);
-      divContent.appendChild(h1);
-      divContent.appendChild(p);
-    }
+  const limit = mealVal.value < 7 ? mealVal.value : 6;
+  for (let i = 0; i < limit; i++) {
+    const recipe = data.results[i];
+
+    const div = document.createElement("div");
+    div.classList.add("w-full", "h-48", "shadow-lg", "flex", "p-4");
+    suggestionBox.appendChild(div);
+
+    const divImg = document.createElement("div");
+    divImg.classList.add("w-1/3", "h-48");
+    div.appendChild(divImg);
+
+    const divContent = document.createElement("div");
+    divContent.classList.add("w-2/3", "h-48");
+    div.appendChild(divContent);
+
+    const img = document.createElement("img");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+
+    h1.textContent = recipe.title;
+    p.textContent = "Calories: " + data.results[i].nutrition.nutrients[0].amount + " kcal";
+    img.src = recipe.image;
+
+    img.classList.add("w-36", "h-36", "rounded-lg");
+    h1.classList.add("pl-4", "pt-4", "text-lg", "font-bold", "header-font");
+    p.classList.add("pl-4", "pt-4", "body-font");
+
+    divImg.appendChild(img);
+    divContent.appendChild(h1);
+    divContent.appendChild(p);
   }
 }
 
+function mealSearch() {
+  const allergies = getAllergies();
+  const diets = getDiets();
+  const cals = getCals();
+  const random = randomMeal();
 
-function mealSearch(getAllergies, getDiets, getCals, randomMeal) {
-  $("#meal-suggestions").empty();
-  
+  const queryURL =
+    "https://api.spoonacular.com/recipes/complexSearch?apiKey=" +
+    key +
+    "&intolerances=" +
+    allergies +
+    "&diet=" +
+    diets +
+    "&maxCalories=" +
+    cals +
+    "&offset=" +
+    random;
 
-  var queryURL =
-    "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + key + "&intolerances=" + getAllergies + "&diet=" + getDiets + "&maxCalories=" + getCals +"&offset=" + randomMeal;
-  {
-    fetch(queryURL, {
-      headers: { "Content-Type": "application/json" },
+  fetch(queryURL, {
+    headers: { "Content-Type": "application/json" },
+  })
+    .then(function (response) {
+      return response.json();
     })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        renderMeals(data)
-      });
-  }
+    .then(function (data) {
+      renderMeals(data);
+    });
 }
 
-//meal handler, add in parameters/arguments for narrowing the search
+// Meal button click handler
 function submitMealHandler() {
-  if (!localStorage.getItem('localUser')) {
+  if (!localStorage.getItem("localUser")) {
     displayModalMeal();
   } else {
-    mealSearch(getAllergies(), getDiets(), getCals(), randomMeal())
+    mealSearch();
   }
 }
 
 function displayModalMeal() {
-  document.getElementById('mealModalOverlay').classList.toggle('hidden');
-  document.getElementById('mealModalOverlay').classList.toggle('flex');
+  const mealModalOverlay = document.getElementById("mealModalOverlay");
+  mealModalOverlay.classList.toggle("hidden");
+  mealModalOverlay.classList.toggle("flex");
 }
 
-document.getElementById('modalOkButton').onclick = function(){location.href = "userForm.html"}
-mealBtn.addEventListener("click", submitMealHandler);
+// Event listeners
+document.getElementById("activityNxBtn").addEventListener("click", function () {
+  location.href = "activityForm.html";
+});
 
+document.getElementById("modalOkButton").onclick = function () {
+  location.href = "userForm.html";
+};
+
+mealBtn.addEventListener("click", submitMealHandler);
